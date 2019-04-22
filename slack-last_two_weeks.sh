46d036de7b53fdde7553419e7937d119e7aa7d57
Matt Bentley <mbentley@mbentley.net>
2019-04-22 14:41:08 -0400

    Updates to streamline script
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- slack-last_two_weeks.sh
+++ slack-last_two_weeks.sh
@@ -12,8 +12,7 @@ SINCE="$(date -d 'now - 2 weeks' +'%s')"
 
 # get slack get deactivated users & filter out bots and non-employees
 IFS=$'\n'
-for PERSON in $(curl -s "https://slack.com/api/users.list?token=${TOKEN}&pretty=1" | jq -r --arg SINCE "$SINCE" '.members | .[] | select((.deleted == true) and (.updated >= '$SINCE') and (.is_bot == false) and (.is_restricted == false)) | "\(.updated) \(.profile.real_name)"' | sort)
+for PERSON in $(curl -s "https://slack.com/api/users.list?token=${TOKEN}&pretty=1" | jq -r --arg SINCE "$SINCE" '.members | .[] | select((.deleted == true) and (.updated >= '"$SINCE"') and (.is_bot == false) and (.is_restricted == false)) | "\(.updated) \(.profile.real_name)"' | sort)
 do
-  DATE="$(date -d @"$(echo "${PERSON}" | awk '{print $1}')")"
-  echo "${DATE} -$(echo "${PERSON}" | awk '{first = $1; $1 = ""; print $0}')"
+  echo "$(date -d @"$(echo "${PERSON}" | awk '{print $1}')") -$(echo "${PERSON}" | awk '{first = $1; $1 = ""; print $0}')"
 done
